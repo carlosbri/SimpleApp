891734357d2fe85ba0774ad77dae99a46a86af18
Carlos Brito <carlos.de.brito@outlook.com>
2013-05-15 22:41:59 -0700

    Simple app

--- SimpleApp/ViewController.m
+++ SimpleApp/ViewController.m
@@ -14,6 +14,8 @@
 
 @implementation ViewController
 
+@synthesize userName = _userName;
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
@@ -25,5 +27,34 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (IBAction)changeGreeting:(id)sender {
+    
+    self.userName = self.textField.text;
+    
+    NSString *nameString = self.userName;
+    
+    if ([nameString length] == 0)
+    {
+        nameString = @"World";
+    }
+    
+    NSString *greeting = [[NSString alloc] initWithFormat:@"Hello, %@!", nameString];
+    self.label.text = greeting;
+    
+    [self.textField resignFirstResponder];
+    
+}
+
+- (BOOL) textFieldShouldReturn:(UITextField *)theTextField
+{
+    if (theTextField == self.textField)
+    {
+        [theTextField resignFirstResponder];
+    }
+    
+    return YES;
+}
+
 
 @end
