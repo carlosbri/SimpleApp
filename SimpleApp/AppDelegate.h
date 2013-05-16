21ee7c6bac6bcfd5fff015794667e950fa76e3b4
Carlos Brito <carlos.de.brito@outlook.com>
2013-05-15 23:28:35 -0700

    Commit from command line.
    	modified:   ../SimpleApp.xcodeproj/project.xcworkspace/xcuserdata/carlosbrito.xcuserdatad/UserInterfaceState.xcuserstate
    	modified:   AppDelegate.h

--- SimpleApp/AppDelegate.h
+++ SimpleApp/AppDelegate.h
@@ -3,7 +3,7 @@
 //  SimpleApp
 //
 //  Created by Carlos Brito on 5/14/13.
-//  Copyright (c) 2013 HW. All rights reserved.
+//  Copyright (c) 2013 HW. All rights reserved. 
 //
 
 #import <UIKit/UIKit.h>
